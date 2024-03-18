3202dfd213d1f7cc66fb7956ab59480d3e704d39
pansee mohamed badry <158486014+PanseeBadry@users.noreply.github.com>
2024-03-18 18:42:43 +0200

    connection.js

--- database/connection.js
+++ database/connection.js
@@ -3,10 +3,7 @@ import mongoose from "mongoose"
 
 
 export const connection = ()=>{
-    mongoose.connect(process.env.DB_ONLINE,{
-         socketTimeoutMS: 30000,
-         connectTimeoutMS: 30000
-    })
+    mongoose.connect(process.env.DB_ONLINE)
     .then(()=>console.log("db connected"))
     .catch((err)=>console.log(err))
 }
